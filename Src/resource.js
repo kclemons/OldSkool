c8f84d2be4afb1169d3162a175e81012cba0c053
kclemons <cl3m0ns@gmail.com>
2014-08-02 19:57:23 -0400

    initial commit

---
+++ Src/resource.js
@@ -0,0 +1,23 @@
+var dirArt = "Art/";
+
+var cardBack = dirArt + "cardBack.png";
+var cardSprite = dirArt + "cardSprite.png";
+var spritePlist = dirArt + "spriteData.plist";
+var bg = dirArt + "bg.png";
+var cardSpotHolder = dirArt + "cardSpot.png";
+var aiSpotMarker = dirArt + "aiSpotMarker.png";
+var dealerChipRes = dirArt + "dealerChip.png";
+var currentTurnImg = dirArt + "playerTurnIndicator.png";
+  
+var g_ressources = [
+ 
+    {type:"image", src:cardBack},
+    {type:"image", src:cardSprite},
+    {type:"image", src:bg},
+    {type:"image", src:cardSpotHolder},
+    {type:"plist", src:spritePlist},
+    {type: "image", src:aiSpotMarker},
+    {type: "image", src: currentTurnImg},
+    {type: "image", src: dealerChipRes}
+ 
+];
