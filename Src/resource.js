659e5ac76925d87588aee1401b42c14032635fc9
kclemons <cl3m0ns@gmail.com>
2014-08-02 20:21:44 -0400

    default starter kit build.

--- Src/resource.js
+++ Src/resource.js
@@ -1,23 +1,6 @@
 var dirArt = "Art/";
+var s_player = dirArt + "player.png";
 
-var cardBack = dirArt + "cardBack.png";
-var cardSprite = dirArt + "cardSprite.png";
-var spritePlist = dirArt + "spriteData.plist";
-var bg = dirArt + "bg.png";
-var cardSpotHolder = dirArt + "cardSpot.png";
-var aiSpotMarker = dirArt + "aiSpotMarker.png";
-var dealerChipRes = dirArt + "dealerChip.png";
-var currentTurnImg = dirArt + "playerTurnIndicator.png";
-  
 var g_ressources = [
- 
-    {type:"image", src:cardBack},
-    {type:"image", src:cardSprite},
-    {type:"image", src:bg},
-    {type:"image", src:cardSpotHolder},
-    {type:"plist", src:spritePlist},
-    {type: "image", src:aiSpotMarker},
-    {type: "image", src: currentTurnImg},
-    {type: "image", src: dealerChipRes}
- 
+    { type: "image", src: s_player }
 ];
