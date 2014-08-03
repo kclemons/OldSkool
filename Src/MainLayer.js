4638907de631c6d6c5eda5d800975bd7c843a22d
kclemons <cl3m0ns@gmail.com>
2014-08-03 15:21:57 -0400

    border of tile map detection and player movement's working well

--- Src/MainLayer.js
+++ Src/MainLayer.js
@@ -2,159 +2,150 @@ var audioEngine = cc.AudioEngine.getInstance();
 
 var MainLayer = cc.LayerColor.extend({
 
-    _monsters:[],
-    _projectiles:[],
-    _monstersDestroyed:0,
-
-    ctor:function() {
+    _monsters: [],
+    _projectiles: [],
+    _monstersDestroyed: 0,
+    _tileMap: null,
+    keyboardArrows: {},
+    speed: 5,
+    ctor: function () {
 
         // Rest of file...
         this._super();
 
-        cc.associateWithNative( this, cc.LayerColor );
+        cc.associateWithNative(this, cc.LayerColor);
     },
 
-    onEnter:function () {
+    onEnter: function () {
         this._super();
 
-        if( 'touches' in sys.capabilities ) {
+        if ('touches' in sys.capabilities) {
             this.setTouchEnabled(true);
         }
-        if( 'mouse' in sys.capabilities ) {
+        if ('mouse' in sys.capabilities) {
             this.setMouseEnabled(true);
         }
+        this.setKeyboardEnabled(true);
+        this.keyboardArrows = {
+            left: false,
+            right: false,
+            up: false,
+            down: false
+        }
+        this._tileMap = cc.TMXTiledMap.create(s_tilemap);
+        var xCord = 0;
+        var yCord = (this._tileMap.height / 2) / (winSize.height / 2);
+        this._tileMap.setPosition(xCord, yCord);
+        var collidableLayer = this._tileMap.getLayer("collision");
 
-        var myTileMap = cc.TMXTiledMap.create(s_tilemap);
-        myTileMap.setPosition(myTileMap.getContentSize().width / 2, winSize.height / 2);
-        this.addChild(myTileMap);
+        this.addChild(this._tileMap);
         var player = cc.Sprite.create(s_player);
-        player.setPosition(player.getContentSize().width / 2, winSize.height / 2);
+        player.setPosition(winSize.width / 2, winSize.height / 2);
         this.addChild(player);
 
-        this.schedule(this.gameLogic, 3);
         this.scheduleUpdate();
 
-        audioEngine.playMusic(s_bgMusic, true);
+    },
+    locationTapped: function (location) {
+       
     },
 
-    addMonster:function() {
-
-        var monster = cc.Sprite.create(s_monster);
-
-        // Determine where to spawn the monster along the Y axis
-        var minY = monster.getContentSize().height / 2;
-        var maxY = winSize.height - monster.getContentSize().height / 2;
-        var rangeY = maxY - minY;
-        var actualY = (Math.random() * rangeY) + minY; // 1
-
-        // Create the monster slightly off-screen along the right edge,
-        // and along a random position along the Y axis as calculated above
-        monster.setPosition(winSize.width + monster.getContentSize().width/2, actualY);
-        this.addChild(monster); // 2
-
-        // Determine speed of the monster
-        var minDuration = 2.0;
-        var maxDuration = 4.0;
-        var rangeDuration = maxDuration - minDuration;
-        var actualDuration = (Math.random() % rangeDuration) + minDuration;
-
-        // Create the actions
-        var actionMove = cc.MoveTo.create(actualDuration, cc.p(-monster.getContentSize().width/2, actualY)); // 3
-        var actionMoveDone = cc.CallFunc.create(function(node) { // 4
-            cc.ArrayRemoveObject(this._monsters, node); // 5
-            node.removeFromParent();
-
-            var scene = GameOver.scene(false);
-            cc.Director.getInstance().replaceScene(scene);
-        }, this);
-        monster.runAction(cc.Sequence.create(actionMove, actionMoveDone));
-
-        // Add to array
-        monster.setTag(1);
-        this._monsters.push(monster);
-
-    },
-
-    gameLogic:function(dt) {
-        this.addMonster();
-    },
-
-    locationTapped:function(location) {
-        // Set up initial location of the projectile
-        var projectile = cc.Sprite.create(s_projectile);
-        projectile.setPosition(20, winSize.height/2);
-
-        // Determine offset of location to projectile
-        var offset = cc.pSub(location, projectile.getPosition());
-
-        // Bail out if you are shooting down or backwards
-        if (offset.x <= 0) return;
-
-        // Ok to add now - we've double checked position
-        this.addChild(projectile);
-
-        // Figure out final destination of projectile
-        var realX = winSize.width + (projectile.getContentSize().width / 2);
-        var ratio = offset.y / offset.x;
-        var realY = (realX * ratio) + projectile.getPosition().y;
-        var realDest = cc.p(realX, realY);
-
-        // Determine the length of how far you're shooting
-        var offset = cc.pSub(realDest, projectile.getPosition());
-        var length = cc.pLength(offset);
-        var velocity = 480.0;
-        var realMoveDuration = length / velocity;
-
-        // Move projectile to actual endpoint
-        projectile.runAction(cc.Sequence.create(
-            cc.MoveTo.create(realMoveDuration, realDest),
-            cc.CallFunc.create(function(node) {
-                cc.ArrayRemoveObject(this._projectiles, node);
-                node.removeFromParent();
-            }, this)
-        ));
-
-        // Add to array
-        projectile.setTag(2);
-        this._projectiles.push(projectile);
-
-        audioEngine.playEffect(s_shootEffect);
-    },
-
-    onMouseUp:function (event) {
+    onMouseUp: function (event) {
         var location = event.getLocation();
         this.locationTapped(location);
     },
 
-    onTouchesEnded:function (touches, event) {
+    onTouchesEnded: function (touches, event) {
         if (touches.length <= 0)
             return;
         var touch = touches[0];
         var location = touch.getLocation();
         this.locationTapped(location);
     },
-
-    update:function (dt) {
-        for (var i = 0; i < this._projectiles.length; i++) {
-            var projectile = this._projectiles[i];
-            for (var j = 0; j < this._monsters.length; j++) {
-                var monster = this._monsters[j];
-                var projectileRect = projectile.getBoundingBox();
-                var monsterRect = monster.getBoundingBox();
-                if (cc.rectIntersectsRect(projectileRect, monsterRect)) {
-                    cc.log("collision!");
-                    cc.ArrayRemoveObject(this._projectiles, projectile);
-                    projectile.removeFromParent();
-                    cc.ArrayRemoveObject(this._monsters, monster);
-                    monster.removeFromParent();
-
-                    this._monstersDestroyed++;
-                    if (this._monstersDestroyed >= 2) {
-                        var scene = GameOver.scene(true);
-                        cc.Director.getInstance().replaceScene(scene);
-                    }
-                }
-            }
+    moveLeft: function () {
+        var xCord = this._tileMap._position.x;
+        var yCord = this._tileMap._position.y;
+        var spd = this.speed;
+        if (xCord !== 0) {
+            this._tileMap.setPosition(xCord + spd, yCord);
+        }
+    },
+    moveRight: function () {
+        var xCord = this._tileMap._position.x;
+        var yCord = this._tileMap._position.y;
+        var spd = this.speed;
+        if (xCord !== -Math.abs(this._tileMap._contentSize.width / 2)) {
+            this._tileMap.setPosition(xCord - spd, yCord);
+        }
+    },
+    moveUp: function () {
+        var xCord = this._tileMap._position.x;
+        var yCord = this._tileMap._position.y;
+        var spd = this.speed;
+        if (!(yCord <= -Math.abs(this._tileMap._contentSize.height - winSize.height))) {
+            this._tileMap.setPosition(xCord, yCord - spd);
+        }
+    },
+    moveDown: function () {
+        var xCord = this._tileMap._position.x;
+        var yCord = this._tileMap._position.y;
+        var spd = this.speed;
+        if (!(yCord >= 0)) {
+            this._tileMap.setPosition(xCord, yCord + spd);
+        }
+    },
+    onKeyDown: function (e) {
+        //make a case statement
+        switch(e) {
+            case cc.KEY.w:
+                this.keyboardArrows.up = true;
+                break;
+            case cc.KEY.s:
+                this.keyboardArrows.down = true;
+                break;
+            case cc.KEY.a:
+                this.keyboardArrows.left = true;
+                break;
+            case cc.KEY.d:
+                this.keyboardArrows.right = true;
+                break;
+            default:
+                //do nothing
+                break;
+        }
+    },
+    onKeyUp : function(e) {
+        switch (e) {
+            case cc.KEY.w:
+                this.keyboardArrows.up = false;
+                break;
+            case cc.KEY.s:
+                this.keyboardArrows.down = false;
+                break;
+            case cc.KEY.a:
+                this.keyboardArrows.left = false;
+                break;
+            case cc.KEY.d:
+                this.keyboardArrows.right = false;
+                break;
+            default:
+                //do nothing
+                break;
+        }
+    },
+    update: function (dt) {
+        pos = this._tileMap.getPosition();
+        if (this.keyboardArrows.left) {
+            this.moveLeft();
+        }
+        if (this.keyboardArrows.right) {
+            this.moveRight();
+        }
+        if (this.keyboardArrows.up) {
+            this.moveUp();
+        }
+        if (this.keyboardArrows.down) {
+            this.moveDown();
         }
     }
 
--- Src/resource.js
+++ Src/resource.js
@@ -5,31 +5,13 @@ var s_player = dirArt + "player.png";
 var s_monster = dirArt + "monster.png";
 var s_projectile = dirArt + "projectile.png";
 
-var s_bgMusic = dirSounds + "background-music.mp3";
-var s_bgMusicOgg = dirSounds + "background-music.ogg";
-var s_bgMusicCaf = dirSounds + "background-music.caf";
-
-var s_shootEffect = dirSounds + "pew-pew-lei.mp3";
-var s_shootEffectOgg = dirSounds + "pew-pew-lei.ogg";
-var s_shootEffectWav = dirSounds + "pew-pew-lei.wav";
-
 var s_tilemap = dirArt + "tilemap.tmx";
 var s_tilepng = dirArt + "tilesheet.png";
 
 var g_ressources = [
-
     {type:"image", src:s_player},
     {type:"image", src:s_monster},
     { type: "image", src: s_projectile },
     { type: "tmx", src: s_tilemap },
-    { type: "image", src: s_tilepng },
-
-    {type:"sound", src:s_bgMusic},
-    {type:"sound", src:s_bgMusicOgg},
-    {type:"sound", src:s_bgMusicCaf},
-
-    {type:"sound", src:s_shootEffect},
-    {type:"sound", src:s_shootEffectOgg},
-    {type:"sound", src:s_shootEffectWav}
-
+    { type: "image", src: s_tilepng }
 ];
